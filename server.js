e29044f0297fc4019eed91c40bc7b466cb80faf0
tushru2004 <tdt7348@rit.edu>
2015-11-15 22:54:10 -0500

    Added post for Products

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var products = [{
-	    id:1,
-	    description : 'Unbreakble tempered glass bottle',
-	    manufacturer : 'Pyrex'
-	},
-	{
-	    id:2,
-	    description: 'Stainless steel chef\'s pan',
-	    manufacturer : 'All Clad'	
-	}];
+var products = [];
+var productnextid = 1;
 
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
 	res.send('Todo API Root');
@@ -40,6 +35,21 @@ app.get('/products/:id',function(req,res){
 	else{
 		res.status(404).send();
 	}
+});
+
+app.post('/products',function(req,res){
+	var body=req.body;
+
+	//add id field 
+	body.id = productnextid++;
+
+	//push body into array
+	products.push(body);
+
+	console.log('description '+body.description);
+
+	res.json(body);
+
 });
 
 app.listen(PORT ,function(){
