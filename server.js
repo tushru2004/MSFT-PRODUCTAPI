a1492182035c3e9765416b4787528899e6516d41
tushru2004 <tdt7348@rit.edu>
2015-11-28 01:13:31 -0500

    Added jwt

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/users/login',function(req,res){
 	var body = _.pick(req.body,'email','password');
 
 	db.User.authenticate(body).then(function(user){
-		res.json(user.toPublicJSON());
+		res.header('Auth',user.generateToken('authentication')).json(user.toPublicJSON());
 	},function(err){
 		res.status(401).send();
 	});
